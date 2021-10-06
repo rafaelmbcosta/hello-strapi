bcb88ba597432cb325a4e3108f1c2a83691b7b83
Rafael Costa <rafaelmbcosta@gmail.com>
2021-10-06 17:28:19 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3b477fb3ed985b56ce9e93e8b460dc5f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f60033d7-a7a3-4bcf-8f6b-6276567475aa'
+};
